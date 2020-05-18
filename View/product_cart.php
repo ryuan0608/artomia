677215ac7d3fd1afe4be85727c1fda19a75344fe
ryuan0608 <rongyuan0608@gmail.com>
2020-05-17 18:40:59 -0700

    Add cart page.

--- View/product_cart.php
+++ View/product_cart.php
@@ -1,51 +1,43 @@
-<?
-	include "../Model/functions.php";
-	include "./partials/header.php";
+<?php
+    include_once "../Model/functions.php";
+    include_once "partials/header.php";
 
-	$cartItems = getCartItems();
-
+    $cartItems = getCartItems();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Fruit Store</title>
+     <meta name="viewport" content="width=device-width">
+     <!-- <link rel="stylesheet" href="css/styleguide.css"> -->
+     <!-- <link rel="stylesheet" href="css/gridsystem.css"> -->
+     <!-- <link rel="stylesheet" href="css/storetheme.css"> -->
+     <!-- <link rel="stylesheet" href="css/select.css"> -->
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/functions.js"></script>
+    <script src="js/select.js"></script>
+    <script>
+        setUpProductListPage();
+    </script>
+    <title>Artomia Gallery</title>
 </head>
 <body>
-    <div class="container">
-        <nav class="nav-crumbs" style="margin:1em 0">
-            <ul>
-                <li><a href="product_list.php">Back</a></li>
-            </ul>
-        </nav>
-        <div class="grid gap">
-            <div class="col-xs-12 col-md-8">
-                <div class="card flat">
-                <?php
-                	for($i = 0; $i < count($cartItems); $i++) {
-                		echo "id: " . $cartItems[$i]->id;
-                		echo "amount: " . $cartItems[$i]->amount;
-                		echo "price: " . $cartItems[$i]->price;
-
-                		echo "<br>";
-                	}
-                ?>
-                </div>
-            </div>
-            <div class="col-xs-12 col-md-4">
-                <div class="card flat">
-                    
-                    <div class="card-section">
-                        <a href="product_checkout.php" class="form-button confirm">Checkout</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+    <div class="xs-small md-medium" style="height: 1000px; width: 83.33%; margin: auto;  margin-top: 200px;">
+        <?php
+            for($i = 0; $i < count($cartItems); $i++) {
+                $item = $cartItems[$i];
+                echo "<div class='cart_item_lg'>" .
+                        "<div class='cart_image_lg'><img class='thumbnail_lg' src='" . $item->image . "'/></div>" . 
+                        "<div class='cart_info_lg'>" .
+                            "<strong class='product_title_lg'>" . $item->name . "</strong><br>" .
+                            "<label class='product_amount_lg'>" . "x" . $item->amount . "</label><br>" .
+                            "<label class='product_subtotal_lg'>" . "$" . $item->total . "</label>" .
+                        "</div>" .
+                    "</div><hr>";
+           }
+        ?>
+    </div>      
 </body>
-<?
-	include "./partials/footer.php";
-?>
+
+<footer><? include_once "partials/footer.php"; ?></footer>
 </html>
